defb560db1879ba9b1149466e90c23ac3f0b531c
Justin Dubs <jtdubs@gmail.com>
2019-09-24 14:06:25 -0400

    speed up flash script

--- flash.sh
+++ flash.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-(sleep 2; echo L; cat out.hex; echo ""; sleep 2) | microcom -s 115200 -p /dev/arduino_eeprom 
+(sleep 2; echo L; cat out.hex; echo ""; sleep 2) | microcom -s 115200 -p /dev/arduino_eeprom
