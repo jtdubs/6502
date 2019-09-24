2d156f13e7458909e07e593e19615ec878a2d5d1
Justin Dubs <jtdubs@gmail.com>
2019-09-24 12:51:33 -0400

    add delay functions

--- test.s
+++ test.s
@@ -131,6 +131,32 @@ _cleanup:
 
 
 ;;
+;; delay: Delay by A NOPs (1 NOP = 2us)
+;;
+.scope
+delay:
+	dec
+	bne delay
+	rts
+.scend
+
+
+;;
+;; delay_long: Delay by A * 255 NOPs (1 NOP = 2us)
+;;
+.scope
+delay_long:
+	ldx #255
+_loop:
+	dex
+	bne _loop
+	dec
+	bne delay_long
+	rts
+.scend
+
+
+;;
 ;; print_message: Print the "messaage" to the dislay
 ;;
 .scope
