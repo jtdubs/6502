c9474a9ffdfab460a0f9044b755a0c214479b5a6
Justin Dubs <jtdubs@gmail.com>
2019-10-04 14:14:46 -0400

    start of game framework.  intro message and 4fps repaint loop.

--- rom/entry.s
+++ rom/entry.s
@@ -23,24 +23,22 @@
 ;; on_reset: Main Entry Point
 ;;
 .scope
-.data ram
-.space VAR_MESSAGE_IDX  1
-.space VAR_ADVANCE      1
+.data zp
+.space VAR_ADVANCE 1
+
+.data zp
+.space VAR_LINE_1  17
+.space VAR_LINE_2  17
 
 .text
-messages:
-    .word message1
-    .word message2
-    .word message3
-    .word message4
-    .word 0
-message1: .byte "Justin Dubs     ",0
-message2: .byte "Leslie Dubs     ",0
-message3: .byte "Anya Dubs       ",0
-message4: .byte "Miles Dubs      ",0
+intro1: .byte "  Trivial Game  ",0
+intro2: .byte " by Justin Dubs ",0
 
 .text
 on_reset:
+    ;; disable interrupts until after init sequence
+    sei
+
     ;; initialize stack pointer
     ldx #$ff
     txs
@@ -52,19 +50,30 @@ on_reset:
 
     ;; initialize variables
     stz VAR_IRQ_COUNTER
-    stz VAR_MESSAGE_IDX
     stz VAR_ADVANCE
 
-    ;; print message
-    lda VAR_MESSAGE_IDX
-    asl
-    tax
-    lda messages,x
-    sta VAR_MESSAGE_PTR
-    inx
-    lda messages,x
-    sta [VAR_MESSAGE_PTR+1]
-    jsr dsp_print_1
+    ; initialize display buffers
+    lda #'a
+    ldy 16
+_clear_line_1:
+    dey
+    sta VAR_LINE_1,y
+    bne _clear_line_1
+    stz [VAR_LINE_1+16]
+
+    lda #'A
+    ldy 16
+_clear_line_2:
+    dey
+    sta VAR_LINE_2,y
+    bne _clear_line_2
+    stz [VAR_LINE_2+16]
+
+    ;; set timer to fire every 50000 cycles (50ms)
+    lda #[<50000]
+    sta REG_T1C_L
+    lda #[>50000]
+    sta REG_T1C_H
 
     ;; set timer 1 to continuous, free-run mode
     lda #$40
@@ -74,20 +83,38 @@ on_reset:
     lda #[IER_SET | IRQ_TIMER_1]
     sta REG_IER
 
-    ;; set timer to fire every 50000 cycles (50ms)
-    lda #[<50000]
-    sta REG_T1C_L
-    lda #[>50000]
-    sta REG_T1C_H
+    ;; print intro message
+    lda #[<intro1]
+    sta [VAR_MESSAGE_PTR+0]
+    lda #[>intro1]
+    sta [VAR_MESSAGE_PTR+1]
+    jsr dsp_print_1
 
+    lda #[<intro2]
+    sta [VAR_MESSAGE_PTR+0]
+    lda #[>intro2]
+    sta [VAR_MESSAGE_PTR+1]
+    jsr dsp_print_2
+
+_enter_intro_loop:
     ;; enable interrupts
-_enter_loop:
     cli
 
-_loop:
-    ;; spin until VAR_ADVANCE is set
+_intro_loop:
+    ;; wait for 4 seconds (16 * 250ms)
     lda VAR_ADVANCE
-    beq _loop
+    cmp #16
+    bcc _intro_loop
+
+_enter_game_loop:
+    ;; enable interrupts
+    cli
+
+_game_loop:
+    ;; wait for 250ms
+    lda VAR_ADVANCE
+    cmp #$01
+    bcc _game_loop
 
     ;; disable interrupts
     sei
@@ -95,25 +122,23 @@ _loop:
     ;; reset VAR_ADVANCE
     stz VAR_ADVANCE
 
-    ;; go to next message
-    lda VAR_MESSAGE_IDX
-    inc
-    and #$03
-    sta VAR_MESSAGE_IDX
+    ;; blah
+    inc VAR_LINE_1
 
-    ;; refresh the display
-    asl
-    tax
-    lda messages,x
-    sta VAR_MESSAGE_PTR
-    inx
-    lda messages,x
+    ;; re-paint display
+    lda #[<VAR_LINE_1]
+    sta [VAR_MESSAGE_PTR+0]
+    lda #[>VAR_LINE_1]
     sta [VAR_MESSAGE_PTR+1]
+    jsr dsp_print_1
 
-    jsr dsp_home
+    lda #[<VAR_LINE_2]
+    sta [VAR_MESSAGE_PTR+0]
+    lda #[>VAR_LINE_2]
+    sta [VAR_MESSAGE_PTR+1]
     jsr dsp_print_2
 
-    jmp _enter_loop
+    jmp _enter_game_loop
 .scend
 
 
@@ -121,7 +146,7 @@ _loop:
 ;; on_irq
 ;;
 .scope
-.data ram
+.data zp
 .space VAR_IRQ_COUNTER  1
 
 .text
@@ -129,19 +154,33 @@ on_irq:
     ;; save accum value
     pha
 
+    ;; dispatch the interrupt
+    lda REG_IFR
+    bpl _end
+    and REG_IER
+    asl
+    bmi _timer_1
+    jmp _not_timer_1
+
+_timer_1:
     ;; clear the interrupt
-    lda #$ff
-    sta REG_IFR
+    lda REG_T1C_L
 
     ;; increment the counter
     inc VAR_IRQ_COUNTER
     lda VAR_IRQ_COUNTER
 
-    ;; if 1s has elapsed, set VAR_ADVANCE and clear the IRQ_COUNTER
-    cmp #20
-    bne _end
+    ;; if 250ms has elapsed, set VAR_ADVANCE and clear the IRQ_COUNTER
+    cmp #5
+    bcc _end
     inc VAR_ADVANCE
     stz VAR_IRQ_COUNTER
+
+_not_timer_1:
+    ;; clear the interrupt
+    lda #$ff
+    sta REG_IFR
+    jmp _end
 
 _end:
     ;; restore accum value and return
